91d4f4561a56c60d1894433dcbf5dd63c8395a1b
Ryo77Saeba <ryo77saeba73@gmail.com>
2026-07-18 14:55:12 +0200

    autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,16 +1,9 @@
-Bash
-
 #!/bin/bash
-
-echo $(dirname $0)
 
 # Installation de la bibliothèque requise
 python3 -m pip install requests
 
-# Déplacement dans le dossier des scripts
-cd $(dirname $0)/scripts/
+# Exécution du script Python
+python3 $(dirname $0)/scripts/youtube_m3ugrabber.py
 
-# Génération du fichier directement dans le dossier .Stream
-python3 youtube_m3ugrabber.py > ../.Stream/youtube.m3u
-
-echo m3u grabbed et enregistré dans .Stream
+echo "Processus de récupération terminé !"
