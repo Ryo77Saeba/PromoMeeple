253e16d15aac057084a2535b8fa9731ad282729f
Ryo77Saeba <ryo77saeba73@gmail.com>
2026-07-18 14:57:54 +0200

    autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,4 +1,11 @@
+@echo off
+:: Installation de la bibliothèque requise si elle n'est pas présente
 pip install requests
 
-cd scripts/
-python youtube_m3ugrabber.py > ../youtube.m3u
+:: Déplacement dans le dossier des scripts et exécution silencieuse
+cd scripts
+python youtube_m3ugrabber.py
+
+echo.
+echo Processus de recuperation termine avec succes !
+pause
