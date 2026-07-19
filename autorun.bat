aa688f6290b6465ae09094c1af5054c092fbe3c2
Ryo77Saeba <ryo77saeba73@gmail.com>
2026-07-19 12:03:17 +0200

    Supp

--- autorun.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-python -m pip install requests yt-dlp
-
-cd scripts
-python youtube_m3ugrabber.py
-pause
--- autorun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Installation de yt-dlp et de la bibliothèque requests
-python3 -m pip install --upgrade pip
-python3 -m pip install requests yt-dlp
-
-# Exécution du script
-python3 $(dirname $0)/scripts/youtube_m3ugrabber.py
-echo "Processus terminé !"
